0bebdaa299dd161482514429bb06397f6b76d020
creatersapp <vinaykumar752575@gmail.com>
2023-08-17 09:25:19 +0530

    ui fixes

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id='projects' className='text-white text-center flex flex-col justify-center items-center'>
+    <div id='projects' className='text-white antialiased text-center flex flex-col justify-center items-center'>
         <h3 className=' font-semibold text-4xl tracking-wide text-primary-200 mb-10'>Project's</h3>
         <div className='project_container mb-40  flex justify-center items-center flex-col gap-20'>
             <div className='tools CatGO flex justify-start items-center sm:flex-row flex-col hover:bg-primary-300 transition-all rounded-lg p-2 w-full'>
@@ -24,19 +24,19 @@ const Projects = () => {
                     <Link href="https://moderncss.vercel.app" className='text-xs text-blue-300 '>Visit The Project &#8594;</Link>
                 </div>
             </div>
-            <div className='hidden ModernCssAbout h-80 tools CatGO justify-around items-center sm:flex-row flex-col bg-primary-300 transition-all rounded-lg px-4 w-full'>
-                <div className='w-1/3 border-r-2 border-slate-500'>
-                <div className='ToolsUsed text-xl font-medium tracking-wide'>Tool's Used 🛠️
+            <div className='hidden ModernCssAbout mx-16 sm:h-80 h-96 tools CatGO justify-around items-center sm:flex-row flex-col bg-cyan-300 transition-all rounded-lg'>
+                <div className='sm:w-1/3 w-auto sm:border-r-2 border-b-2 pb-10 border-slate-200'>
+                <div className='ToolsUsed text-xl font-semibold tracking-normal text-black drop-shadow-sm'>Tool's Used 🛠️
                 <div className='ToolsUsedContainer'>
-                    <ul className='text-base font-normal tracking-normal text-gray-400'>
+                    <ul className='text-base font-normal tracking-normal text-gray-800 mt-2'>
                         <li>NextJs</li>
                         <li>TailWindCss</li>
                         <li>MongoDB Atlas</li>
                     </ul>
                 </div>
                 </div></div>
-                <div className='AboutProject w-1/2 text-xl font-medium'>About The Project ⓘ
-                <p className='text-base text-gray-400'>A place where you can get new & different styles for making the designing process of any application faster</p>
+                <div className='AboutProject sm:w-1/2 w-auto text-xl font-semibold px-4 text-black'>About The Project ⓘ
+                <p className='text-base text-gray-800 mt-2 font-light '>A place where you can get new & different styles for making the designing process of any application faster</p>
                 </div>
             </div>
         </div>
